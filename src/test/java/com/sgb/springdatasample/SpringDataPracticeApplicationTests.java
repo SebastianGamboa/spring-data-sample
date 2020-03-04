3ce68be247f0954ab0cf23bfaf4f34c93cc86b9b
Sebastián Gamboa <sebasgb123@gmail.com>
2020-03-04 11:00:56 +0100

    first commit

---
+++ src/test/java/com/sgb/springdatasample/SpringDataPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgb.springdatasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
